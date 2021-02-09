ac31c52ac06d681b64a102094c175bc50a6b0469
Paul-Armand Verhaegen <paul-armand.verhaegen@mediahuis.be>
2021-02-09 17:48:20 +0100

    Added oidc_token to authentication methods

--- amundsenatlastypes/client.py
+++ amundsenatlastypes/client.py
@@ -4,18 +4,16 @@ from atlasclient.client import Atlas
 
 
 class AtlasClient:
-    host = os.environ.get('ATLAS_HOST', 'localhost')
-    port = os.environ.get('ATLAS_PORT', 21000)
-    user = os.environ.get('ATLAS_USERNAME', 'admin')
-    password = os.environ.get('ATLAS_PASSWORD', 'admin')
-    timeout = os.environ.get('ATLAS_REQUEST_TIMEOUT', 10)
+    config = {
+        host: os.environ.get('ATLAS_HOST', 'localhost'),
+        port: os.environ.get('ATLAS_PORT', 21000),
+        user: os.environ.get('ATLAS_USERNAME', 'admin'),
+        password: os.environ.get('ATLAS_PASSWORD', 'admin'),
+        oidc_token: os.environ.get('ATLAS_BEARER', None),
+        timeout: os.environ.get('ATLAS_REQUEST_TIMEOUT', 10)
+    }
 
     def driver(self):
-        return Atlas(host=self.host,
-                     port=self.port,
-                     username=self.user,
-                     password=self.password,
-                     timeout=self.timeout)
-
+        return Atlas(**config)
 
 driver = AtlasClient().driver()
